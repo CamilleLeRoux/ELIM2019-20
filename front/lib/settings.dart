a17a23bce2b4f1b1656fd0d8f8080ad3f3a15201
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2019-10-06 16:30:47 +0200

    #2 validation des données + données stockées dans un objet localement

--- front/lib/settings.dart
+++ front/lib/settings.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:intl/intl.dart';
+
+import 'main.dart';
 import 'settingsModel.dart';
 
 class Settings extends StatefulWidget {
@@ -14,6 +17,13 @@ class SettingsFormState extends State<Settings>{
 
   final _formKey = GlobalKey<FormState>();
   static var _settingsModel = new SettingsModel();
+  String _time ="00:00";
+
+  void _changeTime(newTime){
+    setState((){
+      _time = newTime;
+    });
+  }
 
   @override
   Widget build(BuildContext context){
@@ -32,32 +42,46 @@ class SettingsFormState extends State<Settings>{
               children: <Widget>[
                 TextFormField(
                     decoration: InputDecoration(
-                        labelText: 'Entre ton prénom'
-                    )
+                        labelText: 'Prénom'
+                    ),
+                    validator: (value) {
+                     if(value.isEmpty){
+                        return 'Entre ton prénom!';
+                     }
+                     return null;
+                    },
+                    onSaved: (value) => setState(() => _settingsModel.name =
+                        value),
                 ),
                 FlatButton(
                     onPressed: () {
                       DatePicker.showTimePicker(context, showTitleActions: true,
-                          onChanged: (date) {
-                            print('change $date in time zone ' +
-                                date.timeZoneOffset.inHours.toString());
-                          }, onConfirm: (date) {
-                            print('confirm $date');
+                          onConfirm: (date) {
+                            var formatter = new DateFormat("Hm");
+                            _changeTime(formatter.format(date));
+                            _settingsModel.beginningParty = date;
                           }, currentTime: DateTime.now());
+
                     },
                     child: Text(
                       'Choisi l\'heure de début de soirée!',
                       style: TextStyle(color: Colors.blue),
                     )
                 ),
+                Text(
+                  '$_time',
+                  textAlign: TextAlign.center,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
                 TextFormField(
                   decoration: InputDecoration(
-                      labelText: 'Entre le numéros de téléphone d\'un ami'
+                      labelText: 'Téléphone d\'un ami'
                   ),
                   validator: (value) {
                     RegExp exp = new RegExp("^(?:(?:\\+|00)33|0)\\s*[1-9](?:[\\s"
                         ".-]*\\d{2}){4}\$");
-                    if (exp.hasMatch(value)) {
+                    if (!exp.hasMatch(value)) {
                       return 'Ce numéros n\'est pas valide!';
                     }
                     else if(value.isEmpty){
@@ -70,7 +94,7 @@ class SettingsFormState extends State<Settings>{
                 ),
                 TextFormField(
                   decoration: InputDecoration(
-                      labelText: 'Entre l\'adresse où tu vas dormir ce soir!'
+                      labelText: 'Adresse où tu vas dormir ce soir!'
                   ),
                   validator: (value) {
                     if (value.isEmpty) {
@@ -88,8 +112,13 @@ class SettingsFormState extends State<Settings>{
                       form.save();
                       Scaffold
                           .of(context)
-                          .showSnackBar(SnackBar(content: Text('Processing Data')));
-                    }
+                          .showSnackBar(SnackBar(content: Text
+                        ('Enregistrement')));
+                      print("Settings : " + _settingsModel.name + "\nNum " +
+                          _settingsModel.friendsNumber + "\nAdresse " +
+                          _settingsModel.address + "\n Horaire " +
+                          _settingsModel.beginningParty.toString());
+                      }
                   },
                   child: Text('Enregistrer'),
                 ),
